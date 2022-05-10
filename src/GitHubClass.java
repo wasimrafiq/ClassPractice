dd848041987c0f10e404d5d5dec32d67ad9a2f94
wasimrafiq <wasim.rafiq@tekschool.us>
2022-05-10 10:00:22 -0400

    changes after break

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -2,7 +2,10 @@
 public class GitHubClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 10;
+		int b  = 8;
+		int d = a + b;
+		System.out.println(d);
 
 	}
 
